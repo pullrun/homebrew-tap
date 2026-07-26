e07cda936cb33461b1fa0675b4126d560c9c3fdb
YACINE <YACINE@MacBook-Pro-de-MacBouk.local>
2026-07-26 20:16:43 +0100

    bump to 0.7.6

--- Formula/pullrun.rb
+++ Formula/pullrun.rb
@@ -2,15 +2,15 @@ class Pullrun < Formula
   desc "Next-gen container runtime with zero-copy DAG storage and P2P image sync"
   homepage "https://github.com/pullrun/pullrun"
   license "Apache-2.0"
-  version "0.7.5"
+  version "0.7.6"
 
   on_arm do
-    url "https://github.com/pullrun/pullrun/releases/download/v0.7.5/pullrun-0.7.5-darwin-arm64.tar.gz"
-    sha256 "efd1ccd319f0c14ac6df540f99d437fc6ac70ab5106c62304c7b4e4498061105"
+    url "https://github.com/pullrun/pullrun/releases/download/v0.7.6/pullrun-0.7.6-darwin-arm64.tar.gz"
+    sha256 "1cc90ba4b129f6e4644e245831ca5f2b4c5881022df701c89eb252d5aeddfb50"
   end
   on_intel do
-    url "https://github.com/pullrun/pullrun/releases/download/v0.7.5/pullrun-0.7.5-darwin-amd64.tar.gz"
-    sha256 "ca04ea51c26714e201ffd43f4a3f375b1880347d2127e644fb6314103e279a1d"
+    url "https://github.com/pullrun/pullrun/releases/download/v0.7.6/pullrun-0.7.6-darwin-amd64.tar.gz"
+    sha256 "b0842278528c18fcb5ec3a296788b0a2cd4b1f378eba06a629b445e13cb1d94f"
   end
 
   def install
